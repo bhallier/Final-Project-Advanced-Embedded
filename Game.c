7c4680a99d08a0c0e0289229309abda55c8ce07b
Bradley Hallier <bhallier@gmail.com>
2017-12-10 17:02:50 -0500

    Tweaks
    Tweaks and optimizations

--- Game.c
+++ Game.c
@@ -3,20 +3,39 @@
 #include "tm4c123gh6pm.h"
 #include "LCD.h"
 
-uint16_t score,life;
+uint16_t score,life,activeBullets;
 shield* shields[NUMSHIELDS];
+bullet* bullets[10];	//Max bullets stored 10
 shield Shield1,Shield2,Shield3;
+player player1;
 
 void initGame() {
 	int i;
 	score = 0;
 	life = 10;
+	activeBullets = 0;
 	shields[0] = &Shield1;
 	shields[1] = &Shield2;
 	shields[2] = &Shield3;
 	for(i = 0;i < NUMSHIELDS;i++) {
-		shields[i]->x = 19+i*(16+20);	// Used to define equal spacing between shields
-		shields[i]->y = 85;
+		shields[i]->position[0] = 19+i*(16+20);	// Used to define equal spacing between shields
+		shields[i]->position[1] = 85;
 		shields[i]->life = 10;
+	}
+}
+
+void fireBullet(int16_t x, int16_t y){
+	bullet newBullet;
+	newBullet.position[0] = x;
+	newBullet.position[1] = y;
+	bullets[activeBullets] = &newBullet;
+	activeBullets++;
+}
+
+void drawPlayer(){
+	if(player1.position[0] != player1.old_position[0]){
+		BSP_LCD_FillRect(player1.old_position[0],100,10,10,LCD_BLACK);
+		BSP_LCD_FillRect(player1.position[0],100,10,10,LCD_GREEN);
+		player1.old_position[0] = player1.position[0];
 	}
 }
